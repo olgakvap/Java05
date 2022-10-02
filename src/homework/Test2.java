3d34a7d84104334c659b2c8926c565affb7c8726
olgakvap <90151897+olgakvap@users.noreply.github.com>
2022-10-02 12:30:44 -0500

    add HW 10/2/22 HW3

---
+++ src/homework/Test2.java
@@ -0,0 +1,19 @@
+package homework;
+
+public class Test2 {
+
+    public static void  main(String[] args) {
+        int a = 2;
+        int b = 4;
+        if (a >= b) {
+            System.out.println( "Pass");
+        } else {
+            System.out.println( "Fail");
+        }
+    }
+}
+
+
+
+
+
---
+++ src/lessons/Lesson6_if_else_for_loop.java
@@ -0,0 +1,53 @@
+package lessons;
+
+public class Lesson6_if_else_for_loop {
+    public static void main(String[] args){
+        for(int i = 1; i < 6; i++){
+
+            System.out.println("Java for beginners");
+        }
+    }
+}
+
+// print all numvers from 1-5
+//    for(int i = 0; i < 6; i++) {
+//        System.out.println(i);//12345
+//        }
+
+// print all numbers from 5-1
+//      for(int i = 5; i > 0; i--) {
+//        System.out.println(i);//54321
+//        }
+
+//print odd numbers from 1-10
+// for(int i = 1; i < 11; i +=2) {
+//        System.out.println(i);//13579
+//        }
+
+//print odd numbers from -1 to -10
+//for(int i = -1; i >= -10; i -=2) {
+//        System.out.println(i);//-1-3-5-7-9
+//        }
+
+//1-100 with step 10
+//for(int i = 1; i <=100 ; i += 10) {
+//        System.out.println(i);//1,11,21,31,41,51,61,71,81,91
+//        }
+
+
+// 0-100 кратные 15
+//for(int i = 0; i <=100 ; i += 15) {
+//        System.out.println(i);//0,15,30,45,60,75,90
+//        }
+
+//-100 to -100 исключительно кратные 50
+//for(int i = -50; i < 100 ; i += 50) {
+//        System.out.println(i);//-50,0,50
+//        }
+
+// print all letters from а до я включительно Char
+//char letter ='я';
+//System.out.println((int)letter);//буква в виде цифры- я 1103 table ASCII
+//        for(char i ='а'; i <= 'я' ; i++) {
+//        System.out.println(i);/а-я
+
